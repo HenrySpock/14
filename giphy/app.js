24ac427d1086bc6766dbb610995501c3281597b4
Kodai <motiviquestudio@gmail.com>
2022-02-06 01:38:09 -0600

    finished giphy exercise

--- giphy/app.js
+++ giphy/app.js
@@ -1,1 +1,61 @@
 console.log("Let's get this party started!");
+
+// giphy api key: oTSnkrNo0T11r3cs9HmkvZyTP9ND87IZ
+// app: niftygiphy
+// http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=oTSnkrNo0T11r3cs9HmkvZyTP9ND87IZ
+
+// globals
+
+const prevSub = document.getElementById('gifForm');
+const sKey = document.querySelector('#searchKey');
+const clearBtn = document.getElementById('clear');
+
+//event listeners
+prevSub.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log("SUBMIT!");
+    getGifs(sKey.value); 
+    sKey.value = '';
+})
+
+clearBtn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log("SUBMIT!");
+    clear();
+})
+
+// function 
+async function getGifs(searchKey) {
+    let res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchKey}&api_key=oTSnkrNo0T11r3cs9HmkvZyTP9ND87IZ`);
+    // console.log(res.data.data.length);
+    let gifIndex = (Math.floor(Math.random() * (res.data.data.length + 1)));
+    // console.log(gifIndex);
+    let resGif = (res.data.data[gifIndex].url);
+    let embedURL = (res.data.data[gifIndex].embed_url);
+    // console.log(resGif + embedURL);
+    let parent = document.getElementById('newGif');
+    let child = document.createElement('span');    
+    // console.log(parent + "" + child);
+    let addGif = `<iframe src=${embedURL} height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><a href=${resGif}></a>`;
+    // document.getElementById("newGif").innerHTML = addGif;
+    child.innerHTML = addGif;
+    parent.appendChild(child);
+}
+
+function clear(){
+    document.getElementById('newGif').innerHTML = "";
+}
+
+ 
+
+async function getGifs(searchKey) {
+    let res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchKey}&api_key=oTSnkrNo0T11r3cs9HmkvZyTP9ND87IZ`); 
+    let gifIndex = (Math.floor(Math.random() * (res.data.data.length + 1))); 
+    let resGif = (res.data.data[gifIndex].url);
+    let embedURL = (res.data.data[gifIndex].embed_url);  
+    let parent = document.getElementById('newGif');
+    let child = document.createElement('span'); 
+    let addGif = `<iframe src=${embedURL} height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><a href=${resGif}></a>`;
+    child.innerHTML = addGif;
+    parent.appendChild(child);
+}
